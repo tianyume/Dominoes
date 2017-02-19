e6e82cc5e3e1817afd21173aa9cf0efbc1f16af9
Chen Dengle <648728073@qq.com>
2017-02-18 16:09:16 -0800

    Add public variable in DominoController. Finish functions in TileController

--- Assets/Scripts/DominoController.cs
+++ Assets/Scripts/DominoController.cs
@@ -56,6 +56,9 @@ public class DominoController : MonoBehaviour
     };
 
     public DominoFactoryController dominoFactory;
+    public bool IsObservableByAll { get; set; }
+    public GameRole Ownership { get; set; }
+    public DominoPlacement Placement;
 
     delegate void OnClickDelegate(DominoController dominoController);
     OnClickDelegate onClick;
--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TileExtension;
 
 public class TileController : MonoBehaviour
 {
+    const int NUMTILE = 28;
+    const int MAXNUM = 6;
     public DominoController exampleDomino;
-    public List<DominoController> dominoControllers;
+    public List<DominoController> dominoes;
 
 	void Start()
     {
-        dominoControllers = new List<DominoController>(28);
+        dominoes = new List<DominoController>(28);
+        Init();
+        //Shuffle();
+        //ShowCards();
         // Init dominoControllers
 	}
 
@@ -19,20 +25,55 @@ public class TileController : MonoBehaviour
     }
 
     // For Game
-    void Shuffle()
+    public void Shuffle()
     {
-        
+        dominoes.Shuffle();        
     }
 
     // For Game
-    void Reset()
+    public void Reset()
     {
+        Init();
     }
 
     // For Player
-    DominoController Deal()
+    public DominoController Deal(int index)
     {
         //TODO
-        return null;
+        if (dominoes[index].Ownership == GameRole.BoneYard)
+        {
+            return dominoes[index];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    void Init()
+    {
+        if (dominoes.Count>0)
+        {
+            dominoes.Clear();            
+        }
+        for (int i = 0; i <= MAXNUM; i++)
+        {
+            for (int j = i; j <= MAXNUM; j++)
+            {
+                DominoController temp = (DominoController)Instantiate(exampleDomino);
+                temp.SetValues(new DominoController.Values(i,j));
+                dominoes.Add(temp);
+                //temp.transform.position = new Vector3(i*2, j*2, 0);
+            }
+        }       
+    }
+
+    void ShowCards()
+    {
+        int count = 0;
+        foreach (DominoController temp in dominoes)
+        {
+            temp.transform.position = new Vector3(count++, 0, 0);
+        }
     }
 }
